2d434cb6f26814bf0c882af4fa6b868d158e354a
salmanvs <108000153+salmanvs@users.noreply.github.com>
2022-08-04 11:57:30 +0530

    Add files via upload

---
+++ Java-Servlet-Crud/crud/src/main/java/emp/DatabaseConnection.java
@@ -0,0 +1,9 @@
+package emp;
+
+public class DatabaseConnection {
+	public static final String driverClass = "com.mysql.jdbc.Driver";
+
+	public static final String connectionUrl = "jdbc:mysql://localhost:3306/test";
+	public static final String username = "root";
+	public static final String password = "";
+}
---
+++ Java-Servlet-Crud/crud/src/main/java/emp/Editservlet2.java
@@ -0,0 +1,71 @@
+package emp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Editservlet2
+ */
+@WebServlet("/Editservlet2")
+public class Editservlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Editservlet2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String sid = request.getParameter("id");
+		int id = Integer.parseInt(sid);
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+
+		Emp e = new Emp();
+		e.setId(id);
+		e.setName(name);
+		e.setPassword(password);
+		e.setEmail(email);
+		e.setCountry(country);
+
+		int status = EmpDao.update(e);
+		if (status > 0) {
+			response.sendRedirect("Viewservlet");
+		} else {
+			out.println("Sorry! unable to update record");
+		}
+
+		out.close();
+	}
+}
